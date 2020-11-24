c38716ecf4fd65e16401745d3d0352ad37b7d0c6
xjlonly <xjlonly@foxmail.com>
2020-11-24 18:04:35 +0800

    canal service

--- changgou-parent/changgou-service/changgou-service-canal/src/main/java/com/changgou/canal/CanalDataEventListener.java
+++ changgou-parent/changgou-service/changgou-service-canal/src/main/java/com/changgou/canal/CanalDataEventListener.java
@@ -2,17 +2,24 @@ package com.changgou.canal;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.otter.canal.protocol.CanalEntry;
+import com.changgou.config.RabbitmqConfig;
 import com.changgou.content.feign.ContentFeign;
+import com.netflix.discovery.BackupRegistry;
 import com.netflix.discovery.converters.Auto;
 import com.xpand.starter.canal.annotation.*;
 import com.xpand.starter.canal.annotation.ListenPoint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.redis.core.ReactiveRedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.List;
+import java.util.UUID;
 
 @CanalEventListener
 public class CanalDataEventListener {
@@ -24,6 +31,9 @@ public class CanalDataEventListener {
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
 //    /**
 //     *
 //     * 增加监听
@@ -33,15 +43,15 @@ public class CanalDataEventListener {
 //        rowData.getAfterColumnsList().forEach(c-> logger.info("数据列：{}，值：{}",c.getName(),c.getValue()));
 //    }
 //
-//    /***
-//     * 修改数据监听
-//     * @param rowData
-//     */
-//    @UpdateListenPoint
-//    public void onEventUpdate(CanalEntry.RowData rowData) {
-//        logger.info("UpdateListenPoint");
-//        rowData.getAfterColumnsList().forEach((c) -> logger.info("By--Annotation: " + c.getName() + " ::   " + c.getValue()));
-//    }
+    /***
+     * 修改数据监听
+     * @param rowData
+     */
+    @UpdateListenPoint
+    public void onEventUpdate(CanalEntry.RowData rowData) {
+        logger.info("UpdateListenPoint");
+        rowData.getAfterColumnsList().forEach((c) -> logger.info("By--Annotation: " + c.getName() + " ::   " + c.getValue()));
+    }
 //
 //    /***
 //     * 删除数据监听
@@ -63,7 +73,12 @@ public class CanalDataEventListener {
 //        rowData.getAfterColumnsList().forEach((c) -> logger.info("By--Annotation: " + c.getName() + " ::   " + c.getValue()));
 //    }
 
-
+    final RabbitTemplate.ConfirmCallback confirmCallback = new RabbitTemplate.ConfirmCallback() {
+        @Override
+        public void confirm(CorrelationData correlationData, boolean ack, String cause) {
+            logger.info("消息ACK结果:{},correlationData:{}",ack,correlationData.getId());
+        }
+    };
     /***
      * 自定义数据修改监听
      * @param eventType
@@ -72,16 +87,20 @@ public class CanalDataEventListener {
     @ListenPoint(destination = "example",
             schema = "changgou_content", table = {"tb_content","tb_content_category"},
             eventType = {CanalEntry.EventType.UPDATE,CanalEntry.EventType.INSERT, CanalEntry.EventType.DELETE})
-    public void onEventCustomUpdate(CanalEntry.EventType eventType, CanalEntry.RowData rowData) {
+    public void onEventCustomContent(CanalEntry.EventType eventType, CanalEntry.RowData rowData) {
         logger.info("广告数据有变更，更新缓存数据...");
         String categoryId = getColumnValue(eventType, rowData);
         var result = contentFeign.findByCategory(Long.parseLong(categoryId));
         var contents = result.getData();
         stringRedisTemplate.boundValueOps("content_" + categoryId).set(JSON.toJSONString(contents));
+
+        rabbitTemplate.setConfirmCallback(confirmCallback);
+        CorrelationData correlationData = new CorrelationData(UUID.randomUUID().toString());
+        rabbitTemplate.convertAndSend(RabbitmqConfig.EXCHANGE_TOPICS_INFORM, "inform.content", categoryId,correlationData);
     }
 
     /*
-    *
+    *获取分类标识
     * */
     private String getColumnValue(CanalEntry.EventType eventType, CanalEntry.RowData rowData){
         String categoryId = "";
@@ -105,4 +124,46 @@ public class CanalDataEventListener {
         return categoryId;
     }
 
+
+
+    @ListenPoint(destination = "example",
+            schema = "changgou_goods",
+            table = {"tb_spu"},
+            eventType = {CanalEntry.EventType.UPDATE, CanalEntry.EventType.INSERT, CanalEntry.EventType.DELETE})
+    public void onEventCustomSpu(CanalEntry.EventType eventType, CanalEntry.RowData rowData){
+        long spuId = 0L;
+        boolean delete =false;
+        if(eventType == CanalEntry.EventType.DELETE){
+            List<CanalEntry.Column> beforeColumnsList = rowData.getBeforeColumnsList();
+            for(CanalEntry.Column column : beforeColumnsList){
+                if(column.getName().equals("id")){
+                    spuId = Long.parseLong(column.getValue());
+                    delete = true;
+                    break;
+                }
+            }
+        }
+        else{
+            List<CanalEntry.Column> afterColumnsList= rowData.getAfterColumnsList();
+            for(CanalEntry.Column column : afterColumnsList){
+                if(column.getName().equals("id")){
+                    spuId = Long.parseLong(column.getValue());
+                    break;
+                }
+            }
+        }
+
+        rabbitTemplate.setConfirmCallback(confirmCallback);
+        CorrelationData correlationData = new CorrelationData(UUID.randomUUID().toString());
+        rabbitTemplate.convertAndSend(RabbitmqConfig.EXCHANGE_TOPICS_INFORM,"inform.html", JSON.toJSONString(new SpuData(spuId, delete)),correlationData);
+    }
+
+    class SpuData {
+        public final long spuId;
+        public final boolean delete;
+        public SpuData(long spuId, boolean delete){
+            this.spuId = spuId;
+            this.delete = delete;
+        }
+    }
 }
---
+++ changgou-parent/changgou-service/changgou-service-canal/src/main/java/com/changgou/config/RabbitmqConfig.java
@@ -0,0 +1,48 @@
+package com.changgou.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class RabbitmqConfig {
+    public  static final String QUEUE_INFO_CONTENT = "queue_inform_content";
+    public  static final String QUEUE_INFO_HTML = "queue_inform_html";
+    public  static final String EXCHANGE_TOPICS_INFORM = "exchange_topics_inform";
+    public static final String ROUTING_KEY_CONTENT="inform.#.content.#";
+    public static final String ROUTING_KEY_HTML="inform.#.html.#";
+
+    /*
+    * 声明交换机
+    * */
+    @Bean(EXCHANGE_TOPICS_INFORM)
+    public Exchange EXCHANGE_TOPICS_INFORM(){
+        //durable(true) 持久化，mq重启之后交换机还在
+        return ExchangeBuilder.topicExchange(EXCHANGE_TOPICS_INFORM).durable(true).build();
+    }
+
+    //声明QUEUE_INFORM_EMAIL队列
+    @Bean(QUEUE_INFO_CONTENT)
+    public Queue QUEUE_INFO_CONTENT(){
+        return new Queue(QUEUE_INFO_CONTENT,true);
+    }
+    //声明QUEUE_INFORM_SMS队列
+    @Bean(QUEUE_INFO_HTML)
+    public Queue QUEUE_INFO_HTML(){
+        return new Queue(QUEUE_INFO_HTML,true);
+    }
+    //将content队列绑定到交换机并指定routingkey
+    @Bean
+    public Binding binding_queue_inform_content(@Qualifier(QUEUE_INFO_CONTENT) Queue queue, @Qualifier(EXCHANGE_TOPICS_INFORM) Exchange exchange){
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY_CONTENT).noargs();
+    }
+
+    //将content队列绑定到交换机并指定routingkey
+    @Bean
+    public Binding binding_queue_inform_html(@Qualifier(QUEUE_INFO_HTML) Queue queue, @Qualifier(EXCHANGE_TOPICS_INFORM) Exchange exchange){
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY_HTML).noargs();
+    }
+
+}
